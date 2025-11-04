9b4083c6bda9a64f31e73f78e06724cf0f29e94d
moros04 <166436342+moros04@users.noreply.github.com>
2025-11-04 12:29:37 -0800

    Added a login page with boxes for username and password. I also added a continue as guest button if someone wants to do so.
    I also modified some files to be able to redirect to the home page after logging in or continuing as guests

--- App.tsx
+++ App.tsx
@@ -1,9 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from './screens/login';
 import Home from './screens/home';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'CloutCast' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-
